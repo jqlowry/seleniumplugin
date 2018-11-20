553a119056e8669d4c1b6371705a52ff7f274583
jqlowry <Justin.Lowry@mtsi-va.com>
2018-11-20 13:43:34 -0700

    Class rename

--- src/test/java/umloginTest.java
+++ src/test/java/umloginTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class umlogin {
+public class umloginTest {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
